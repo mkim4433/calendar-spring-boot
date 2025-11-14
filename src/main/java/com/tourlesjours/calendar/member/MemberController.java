4c1c32d63b10c35caa50a529fff0e9683d82f698
mkim4433 <mkim4433@gmail.com>
2025-11-14 16:41:28 +0900

    회원 로그인 세션 관리 추가 및 로그아웃 기능 구현

--- src/main/java/com/tourlesjours/calendar/member/MemberController.java
+++ src/main/java/com/tourlesjours/calendar/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.tourlesjours.calendar.member;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -41,11 +42,25 @@ public class MemberController {
 
     // 로그인 확인
     @PostMapping("/signin_confirm")
-    public String signinConfirm(MemberDto memberDto, Model model) {
+    public String signinConfirm(MemberDto memberDto, Model model, HttpSession session) {
 
         String loginedId = memberService.signinConfirm(memberDto);
         model.addAttribute("loginedId", loginedId);
 
+        if (loginedId != null) {
+            session.setAttribute("loginedId", loginedId);
+            session.setMaxInactiveInterval(60 * 30);
+        }
+
         return "member/signin_result";
+    }
+
+    // 로그아웃 확인
+    @GetMapping("/signout_confirm")
+    public String signoutConfirm(HttpSession session) {
+
+        session.invalidate();
+
+        return "redirect:/";
     }
 }
