7e201c8ea76d924f90a7d6754116be3d63346199
mkim4433 <mkim4433@gmail.com>
2026-01-02 21:30:45 +0900

    lombok 설정 및 적용

--- src/main/java/com/tourlesjours/calendar/member/MemberDto.java
+++ src/main/java/com/tourlesjours/calendar/member/MemberDto.java
@@ -1,5 +1,14 @@
 package com.tourlesjours.calendar.member;
 
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+@Data
+
 public class MemberDto {
 
     private int no;
@@ -11,67 +20,4 @@ public class MemberDto {
     private String reg_date;
     private String mod_date;
 
-    public int getNo() {
-        return no;
-    }
-
-    public void setNo(int no) {
-        this.no = no;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPw() {
-        return pw;
-    }
-
-    public void setPw(String pw) {
-        this.pw = pw;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public int getAutority_no() {
-        return autority_no;
-    }
-
-    public void setAutority_no(int autority_no) {
-        this.autority_no = autority_no;
-    }
-
-    public String getReg_date() {
-        return reg_date;
-    }
-
-    public void setReg_date(String reg_date) {
-        this.reg_date = reg_date;
-    }
-
-    public String getMod_date() {
-        return mod_date;
-    }
-
-    public void setMod_date(String mod_date) {
-        this.mod_date = mod_date;
-    }
 }
